0fafafe188539c298bc35518881e1a95e280c21a
googlebrain <googlebrain@163.com>
2020-03-08 16:27:31 +0800

    线程这块部分的代码

---
+++ src/TestLockCondition.java
@@ -0,0 +1,63 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TestLockCondition {
+    public static void main(String[] args) {
+        ABC abc = new ABC();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0;i<5;i++){
+                    abc.A();
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0;i<5;i++){
+                    abc.B();
+                }
+            }
+        }).start();
+    }
+}
+
+
+class ABC{
+    private int a=1;
+    public Lock lock = new ReentrantLock();
+    Condition c1 = lock.newCondition();
+    Condition c2 = lock.newCondition();
+
+    public void A(){
+        lock.lock();
+        if(a!=1){
+            try {
+                c1.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("A");
+        a=2;
+        c2.signal();
+        lock.unlock();
+    }
+    public void B(){
+        lock.lock();
+         if(a!=2){
+             try {
+                 c2.await();
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+         }
+         System.out.println("B");
+         a=1;
+         c1.signal();
+         lock.unlock();
+    }
+}
